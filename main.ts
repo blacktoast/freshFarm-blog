6b843dc9c5fcb99f26d14f43c5a8a2f8296e17a7
blacktoast <dbstjd100@naver.com>
2022-07-07 19:04:55 +0900

    test

--- main.ts
+++ main.ts
@@ -4,11 +4,29 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
+import { compile } from 'mdx2';
+
 import { InnerRenderFunction, RenderContext, start } from '$fresh/server.ts';
 import manifest from './fresh.gen.ts';
 
 import { config, setup } from '@twind';
 import { virtualSheet } from 'twind/sheets';
+
+const dirs = ['posts', 'notes'];
+
+dirs.map(async (dir) => {
+  const path = `./blog/${dir}`;
+  for await (const dirEntry of Deno.readDir(path)) {
+    const body = await Deno.readTextFile(`${path}/${dirEntry.name}`);
+    console.log('aa    ', dirEntry.name.split('.')[0]);
+    const compiled = await compile(body, { jsxImportSource: 'preact' });
+    console.log(compiled);
+    // await Deno.writeTextFile(
+    //   `./mdx/${dir}/${dirEntry.name.split('.')[0]}.jsx`,
+    //   compiled
+    // );
+  }
+});
 
 const sheet = virtualSheet();
 sheet.reset();
