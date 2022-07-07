48b60c453341f12af1d49117c577eeeee09603ba
blacktoast <dbstjd100@naver.com>
2022-07-08 01:05:39 +0900

    test

--- routes/blog.tsx
+++ routes/blog.tsx
@@ -20,12 +20,10 @@ import { Page } from '@/components/Page.tsx';
 // };
 
 export const handler = async (_req: Request, ctx: HandlerContext): Response => {
-  const body = 'aa';
-  // const body = (await import('./mdx/posts/a.jsx')).default;
-  // console.log(body);
-  for await (const dirEntry of Deno.readDir('./')) {
-    console.log(dirEntry);
-  }
+  // const body = 'aa';
+  const body = (await import('./mdx/posts/a.jsx')).default;
+  console.log(body);
+
   // const body = await Deno.readTextFile(
   //   './posts/fresh 로 개발 블로그 구현기2.jsx'
   // );
