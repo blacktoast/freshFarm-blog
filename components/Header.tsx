523e47276734eb5f8ca63cca3b25af1adf970a03
blacktoast <dbstjd100@naver.com>
2022-08-31 00:52:57 +0900

    ✨ Feat/ Header 디자인 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,48 @@
 import { h } from 'preact';
 import { tw } from '@twind';
 
-interface HeaderProps {}
+interface HeaderProps {
+  activeLink?: string;
+}
 
-export const Header = ({}: HeaderProps) => {
+interface linkType {
+  [key: string]: Boolean;
+}
+
+export const Header = ({ activeLink = '' }: HeaderProps) => {
+  const links: linkType = {
+    post: false,
+    notes: false,
+    projects: false,
+    guestBook: false,
+  };
+  if (typeof links[activeLink as string] !== 'undefined') {
+    links[activeLink] = true;
+  }
+
   return (
-    <div class={tw`flex `}>
-      <a href="/post">blog</a>
-      <a href="/notes">note</a>
-      <a href="/projects">projects</a>
-      <a href="/guestBook">guestBook</a>
+    <div
+      class={tw` flex px-8 sticky top-0
+      justify-between rounded-lg h-12 items-center  
+      bg-green backdrop-blur`}
+      style={{ backdropFilter: 'blur(8px)' }}
+    >
+      <h2>retto</h2>
+      <div
+        class={tw`hidden sm:flex 
+       gap-4 text-xl items-center `}
+      >
+        {Object.entries(links).map((link) => {
+          const [key, bool] = link;
+          console.log(key, bool);
+          return <a> {key}</a>;
+        })}
+        <a href="/post">blog</a>
+        <a href="/notes">note</a>
+        <a href="/projects">projects</a>
+        <a href="/guestBook">guestBook</a>
+      </div>
+      <div class={tw`flex sm:hidden`}> hi </div>
     </div>
   );
 };
