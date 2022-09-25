c44a52fc67537eab1d605f8a5c7ec0bb8c76b017
blacktoast <dbstjd100@naver.com>
2022-09-25 23:55:26 +0900

    Feat/ 해더 스타일 업데이트

--- utils/twind.ts
+++ utils/twind.ts
@@ -6,6 +6,16 @@ export const config: Configuration = {
   darkMode: 'class',
   mode: 'silent',
   preflight: false,
-  // deno-lint-ignore no-dupe-keys
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0px 0px 15px 5px rgba(0,0,0,0.1)',
+      },
+      backgroundColor: {
+        white: 'rgba(255,255,255,0.25)',
+        green: 'rgba(134, 239, 172, 0.75)',
+      },
+    },
+  },
 };
 if (IS_BROWSER) setup(config);
