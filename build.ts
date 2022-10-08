944ecdbd830749974c8d2bf5cf43a7a5cad2be55
blacktoast <dbstjd100@naver.com>
2022-10-08 23:51:41 +0900

    Fix/ 폴더에 mdx파일이 없을때 mdxToJsx  빌드가 안되는 버그 수정

--- build.ts
+++ build.ts
@@ -3,7 +3,7 @@ import { compile } from 'mdx2';
 
 import { encode } from 'base64';
 
-import { ensureFile } from 'https://deno.land/std@0.147.0/fs/mod.ts';
+import { ensureFile, ensureDir } from 'https://deno.land/std@0.147.0/fs/mod.ts';
 import gfm from 'https://esm.sh/remark-gfm@3.0.1';
 import remarkFrontmatter from 'https://esm.sh/remark-frontmatter@4?bundle';
 
@@ -91,25 +91,19 @@ export const buildMdx = async () => {
     posts: [],
     notes: [],
   };
+
   const promises = dirs.map(async (dir) => {
     const path = `./${baseDir}/${dir}`;
 
-    // mdx파일의 데이터 값을 가져오기
-    // 해당 파일로 있는 jsx파일이 있는지 찾기
-    // code 복사 기능 넣기, 그렇다면 코드 하이 라이팅 + 카피 기능을 넣어야 함
-    // ``` ``` 이건 파싱을 한다 하더라고 코드하이라이팅은 어떻게 하지??
-    // 가능성이 떨어짐, fresh 프레임워크에서 인터렉션이 가능한건 island 폴더뿐인데 여기서는 children를 받지 못함
-    // 그럼 형제 노드로 가야하나?? 형제 노드로 간다 쳐도, 아이콘은 코드안에 존재 해야하는데 그게 되나??
-    // 파싱할때 ``` 마크다운 위에 컴포넌트를 집어 넣으면 될듯 해서 구현 성공을 한듯 하다.
     for await (const dirEntry of Deno.readDir(path)) {
       const readFileName = `${path}/${dirEntry.name}`;
-      const body = await Deno.readTextFile(`${path}/${dirEntry.name}`);
+      const body = await Deno.readTextFile(readFileName);
       const rawTexts = body.split('\n');
       const [tags, description] = metaTagParsing(rawTexts);
       console.log(tags, description);
       const fileStat = await Deno.stat(`${path}/${dirEntry.name}`);
-      const encodeFileName = encode(dirEntry.name.split('.')[0]);
-      const forWriteFileName = `./routes/blog/${dir}/${encodeFileName}.jsx`;
+      const hashedFileName = encode(dirEntry.name.split('.')[0]);
+      const forWriteFileName = `./routes/blog/${dir}/${hashedFileName}.jsx`;
 
       const fileInfo = {
         title: dirEntry.name,
@@ -121,11 +115,11 @@ export const buildMdx = async () => {
         birthtime: fileStat.birthtime,
       };
 
-      fileNames[dir].push(encodeFileName);
+      fileNames[dir].push(hashedFileName);
 
       newDB = {
         ...newDB,
-        [dir]: { ...newDB[dir], [encodeFileName]: fileInfo },
+        [dir]: { ...newDB[dir], [hashedFileName]: fileInfo },
       };
 
       // console.log(removeExportCodeToComplied(String(compiled.value)));
@@ -139,13 +133,20 @@ export const buildMdx = async () => {
       // console.log(dirEntry.name);
 
       await ensureFile(forWriteFileName);
-      const exisFileContent = await Deno.readFile(forWriteFileName);
+      const existFileContent = await Deno.readFile(forWriteFileName);
       // console.log(`./routes/blog/${dir}/${encodeFileName}.jsx`);
-      const existsFileEditedTime = db[dir][encodeFileName]?.mtime || 0;
+
+      //mapping json 에 미리 값이 없기 문에 없다 라고 인식함
+      //즉 mdxindex파일이 만들어지기 전에 처음 렌더링이 돌때에 dir도 없고 hashedFileName도 없을것
+      // ㅇ
+
+      const existsFileEditedTime = db[dir]
+        ? db[dir][hashedFileName]?.mtime || 0
+        : 0;
 
       if (
         String(existsFileEditedTime) !== String(fileInfo.mtime) ||
-        exisFileContent.length === 0
+        existFileContent.length === 0
       ) {
         const enterBody = hardEnter(rawTexts);
 
@@ -165,8 +166,15 @@ export const buildMdx = async () => {
   });
 
   await Promise.all(promises);
-  await Deno.writeTextFile(`./mdxIndex.json`, JSON.stringify(newDB));
-  const removeFilePromises = dirs.map((dir) => {
+
+  const removeFilePromises = dirs.map(async (dir) => {
+    await ensureDir(`./routes/blog/${dir}`);
+
+    if (!db[dir] || !newDB[dir]) {
+      await Deno.remove(`./routes/blog/${dir}`, { recursive: true });
+      return;
+    }
+
     Object.keys(db[dir]).map(async (key) => {
       if (Object.keys(newDB[dir]).includes(key) === false) {
         const name = db[dir][key].path;
@@ -176,6 +184,8 @@ export const buildMdx = async () => {
     });
   });
 
+  await Deno.writeTextFile(`./mdxIndex.json`, JSON.stringify(newDB));
+
   await Promise.all(removeFilePromises);
   console.log('end');
 
@@ -183,3 +193,5 @@ export const buildMdx = async () => {
 };
 
 // buildMdx();
+
+const makeJSX = () => {};
