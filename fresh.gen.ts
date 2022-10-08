fab15bec099410dec416af6271c477559e4aaf70
blacktoast <dbstjd100@naver.com>
2022-10-08 23:52:59 +0900

    Style/ 헤더 스타일 변경

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,43 +2,43 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/blog.tsx";
-import * as $2 from "./routes/blog/posts/7ZWY7J20.jsx";
-import * as $3 from "./routes/blog/posts/YWY=.jsx";
-import * as $4 from "./routes/blog/posts/ZnJlc2gg66GcIOqwnOuwnCDruJTroZzqt7gg6rWs7ZiE6riw.jsx";
-import * as $5 from "./routes/blog/posts/dGVzdA==.jsx";
-import * as $6 from "./routes/blog/posts/dHg=.jsx";
-import * as $7 from "./routes/index.tsx";
-import * as $8 from "./routes/notes.tsx";
-import * as $9 from "./routes/post.tsx";
-import * as $10 from "./routes/tags/[name].tsx";
-import * as $$0 from "./islands/CopyCode.tsx";
-import * as $$1 from "./islands/Counter.tsx";
-import * as $$2 from "./islands/Input.tsx";
-import * as $$3 from "./islands/Todo.tsx";
+import config from './deno.json' assert { type: 'json' };
+import * as $0 from './routes/[name].tsx';
+import * as $1 from './routes/blog.tsx';
+import * as $2 from './routes/blog/posts/7ZWY7J20.jsx';
+import * as $3 from './routes/blog/posts/YWY=.jsx';
+import * as $4 from './routes/blog/posts/ZnJlc2gg66GcIOqwnOuwnCDruJTroZzqt7gg6rWs7ZiE6riw.jsx';
+import * as $5 from './routes/blog/posts/dGVzdA==.jsx';
+import * as $6 from './routes/blog/posts/dHg=.jsx';
+import * as $7 from './routes/index.tsx';
+import * as $8 from './routes/notes.tsx';
+import * as $9 from './routes/post.tsx';
+import * as $10 from './routes/tags/[name].tsx';
+import * as $$0 from './islands/CopyCode.tsx';
+import * as $$1 from './islands/Counter.tsx';
+import * as $$2 from './islands/Input.tsx';
+import * as $$3 from './islands/Todo.tsx';
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/blog.tsx": $1,
-    "./routes/blog/posts/7ZWY7J20.jsx": $2,
-    "./routes/blog/posts/YWY=.jsx": $3,
-    "./routes/blog/posts/ZnJlc2gg66GcIOqwnOuwnCDruJTroZzqt7gg6rWs7ZiE6riw.jsx":
+    './routes/[name].tsx': $0,
+    './routes/blog.tsx': $1,
+    './routes/blog/posts/7ZWY7J20.jsx': $2,
+    './routes/blog/posts/YWY=.jsx': $3,
+    './routes/blog/posts/ZnJlc2gg66GcIOqwnOuwnCDruJTroZzqt7gg6rWs7ZiE6riw.jsx':
       $4,
-    "./routes/blog/posts/dGVzdA==.jsx": $5,
-    "./routes/blog/posts/dHg=.jsx": $6,
-    "./routes/index.tsx": $7,
-    "./routes/notes.tsx": $8,
-    "./routes/post.tsx": $9,
-    "./routes/tags/[name].tsx": $10,
+    './routes/blog/posts/dGVzdA==.jsx': $5,
+    './routes/blog/posts/dHg=.jsx': $6,
+    './routes/index.tsx': $7,
+    './routes/notes.tsx': $8,
+    './routes/post.tsx': $9,
+    './routes/tags/[name].tsx': $10,
   },
   islands: {
-    "./islands/CopyCode.tsx": $$0,
-    "./islands/Counter.tsx": $$1,
-    "./islands/Input.tsx": $$2,
-    "./islands/Todo.tsx": $$3,
+    './islands/CopyCode.tsx': $$0,
+    './islands/Counter.tsx': $$1,
+    './islands/Input.tsx': $$2,
+    './islands/Todo.tsx': $$3,
   },
   baseUrl: import.meta.url,
   config,
