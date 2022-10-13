18231d26a2172dc2e056b4b5948550174f3e2d9c
blacktoast <dbstjd100@naver.com>
2022-10-13 22:45:15 +0900

    Feat/ codecopy 기능 구현중

--- islands/CopyCode.tsx
+++ islands/CopyCode.tsx
@@ -1,18 +1,24 @@
-import { useRef } from "preact/hooks";
+import { useRef } from 'preact/hooks';
+import { CompleteIcon } from '../components/CompleteIcon.tsx';
+import { CopyIcon } from '../components/CopyIcon.tsx';
 
 interface CopyCodeProps {}
 
 const CopyCode = ({}: CopyCodeProps) => {
   const ref = useRef<HTMLParagraphElement>(null);
+
+  const onClick = () => {};
   const copyCode = () => {
     const targetDom: HTMLParagraphElement = ref.current?.nextSibling
       ?.nextSibling as HTMLParagraphElement;
     console.log(targetDom.innerText);
   };
+
   return (
-    <div ref={ref} class="relative">
-      <code onClick={copyCode} class="code-copy-btn absolute top-0 right-0">
-        여기는 코드 복사 버튼
+    <div ref={ref} class='relative'>
+      <code onClick={copyCode} class='absolute top-2 right-1 text-blue-100'>
+        <CopyIcon />
+        <CompleteIcon></CompleteIcon>
       </code>
     </div>
   );
